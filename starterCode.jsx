e93df96eb26df874ac06fef3d6df0c9fb71c27ab
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-08-10 00:02:11 -0500

    Visual changes

--- starterCode.jsx
+++ starterCode.jsx
@@ -2,6 +2,10 @@ const ATMDeposit = ({ onChange, isDeposit, isValid }) => {
   const choice = ['Deposit', 'Cash Back'];
   console.log(`ATM isDeposit: ${isDeposit}`);
   return (
+    <div className="card">
+      <input id="number-input" type="number" width="200" onChange={onChange}></input>
+      <input type="submit" disabled={!isValid} width="200" value="Submit" id="submit-input"></input>
+    </div>
   );
 };
 
@@ -11,6 +15,7 @@ const Account = () => {
   const [totalState, setTotalState] = React.useState(0);
   const [isDeposit, setIsDeposit] = React.useState(true);
   const [validTransaction, setValidTransaction] = React.useState(false);
+  const [checked, setChecked] = React.useState(false);
   let status = `Account Balance $ ${totalState} `;
   console.log(`Account Rendered with isDeposit: ${isDeposit}`);
   const handleChange = (event) => {
@@ -45,12 +50,28 @@ const Account = () => {
       setIsDeposit(false);
     }
   };
+  function handleHide(event){
+    setChecked(!checked);
+  }
   return (
+    <form onSubmit={handleSubmit}>
+      <div class="btn-group-toggle" data-toggle="buttons">
+        <label class="btn btn-secondary btn-sm">
+          <input type="checkbox" checked autocomplete="off" onChange={handleHide} value=""/> {checked ? "Hide Balance" : "Show Balance"}
+        </label>
+      </div>
+      <div className="form-group">
+        <h2 className="lead" id="total">{checked ? status:"Account Balance $****"}</h2>
         <label>Select an action below to continue</label>
+        <br />
+        <select className="form-control" onChange={(e) => handleModeSelect(e)} name="mode" id="mode-select">
         <option id="no-selection" value=""></option>
         <option id="deposit-selection" value="Deposit">Deposit</option>
         <option id="cashback-selection" value="Cash Back">Cash Back</option>
+        </select>
         {atmMode && (<ATMDeposit onChange={handleChange} isDeposit={isDeposit} isValid={validTransaction}></ATMDeposit>)}
+      </div>
+    </form>
   );
 };
 // ========================================
